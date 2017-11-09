e226738af5a92f95e89f3db47947df02eef6f62b
olga-nest <olga.w.nesterova@gmail.com>
2017-11-09 11:44:11 -0800

    Use uiview animation to expand navigation bar

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     //MARK: Properties
     @IBOutlet weak var navbarHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var navigationBar: UIView!
     
     
     override func viewDidLoad() {
@@ -22,9 +23,16 @@ class ViewController: UIViewController {
     //MARK: Actions
     @IBAction func addButtonPressed(_ sender: UIButton) {
         print("Plus icon pressed")
-        navbarHeightConstraint.constant = 200
+        navBarSpringAnimation()
     }
     
+    func navBarSpringAnimation() {
+        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 0.25, initialSpringVelocity: 5, options: [], animations: {
+            // Values for end state of animation
+            self.navigationBar.frame = CGRect(x: self.navigationBar.frame.minX, y: self.navigationBar.frame.minY, width: self.navigationBar.frame.width, height: 200)
+       })
+
+    }
 
 }
 
