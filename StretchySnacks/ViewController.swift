28acaec6b538d5d0c226ebb46ed61dfcef12936e
olga-nest <olga.w.nesterova@gmail.com>
2017-11-09 11:32:57 -0800

    Change navbar height constraint when tapping +

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var navbarHeightConstraint: NSLayoutConstraint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Actions
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+        print("Plus icon pressed")
+        navbarHeightConstraint.constant = 200
     }
-
+    
 
 }
 
